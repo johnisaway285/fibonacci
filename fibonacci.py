dab261f7fc81262480473fa10f8e2744e2954d4d
jomoshy <johnisaway285@gmail.com>
2024-03-08 20:54:14 -0700

    A basic model, but it's slow when the number gets bigger

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,15 @@
 def main():
     a = int(input("Please enter the number for n: "))
-    condition_det(a)
+    print(condition_det(a))
 
 #This function will determine the number of the fibonacci number
-def condition_det(a_number):
+def fibonacci(a_number):
     if a_number ==0:
         return 0
     elif a_number == 1 or a_number ==2:
         return 1
     else:
-
+        return condition_det(a_number-1)+condition_det(a_number-2)
         
 
 if __name__ == "__main__":
