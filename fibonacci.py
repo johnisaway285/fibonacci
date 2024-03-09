32c5532a0a77fa84b2e946032d187b446e9e571b
jomoshy <johnisaway285@gmail.com>
2024-03-08 21:24:54 -0700

    Completed Everything

--- fibonacci.py
+++ fibonacci.py
@@ -1,16 +1,11 @@
+from math import sqrt
 def main():
     a = int(input("Please enter the number for n: "))
-    print(condition_det(a))
+    print(int(fibonacci(a)))
 
 #This function will determine the number of the fibonacci number
 def fibonacci(a_number):
-    if a_number ==0:
-        return 0
-    elif a_number == 1 or a_number ==2:
-        return 1
-    else:
-        return condition_det(a_number-1)+condition_det(a_number-2)
-        
+    return ((1+sqrt(5))**a_number-(1-sqrt(5))**a_number)/(2**a_number*sqrt(5))    
 
 if __name__ == "__main__":
     main()
