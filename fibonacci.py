c648a444a62b89af40895b449f42404b1566af97
jomoshy <johnisaway285@gmail.com>
2024-03-08 20:44:44 -0700

    Added multiple functions

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def main():
+    a = int(input("Please enter the number for n: "))
+    condition_det(a)
+
+def condition_det(a_number):
+    if a_number ==0:
+        return 0
+    elif a_number == 1 or a_number ==2:
+        return 1
+    else:
+        
+
+if __name__ == "__main__":
+    main()
