86187c3466de08e7d879008adf43a518d22c5957
jomoshy <johnisaway285@gmail.com>
2024-03-08 21:43:40 -0700

    added a few commentes for code blocks

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,15 @@
+# Import math libary, sqrt does not come with python :(
 from math import sqrt
+
+#main function
 def main():
     a = int(input("Please enter the number for n: "))
     print(int(fibonacci(a)))
 
-#This function will determine the number of the fibonacci number
+#Core function
 def fibonacci(a_number):
     return ((1+sqrt(5))**a_number-(1-sqrt(5))**a_number)/(2**a_number*sqrt(5))    
 
+#You know the drill
 if __name__ == "__main__":
     main()
