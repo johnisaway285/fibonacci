9cc96453a2c5b07fb235f3b0f1ae36a9cbbab51b
jomoshy <johnisaway285@gmail.com>
2024-03-08 20:47:47 -0700

    Added a comment

--- fibonacci.py
+++ fibonacci.py
@@ -2,12 +2,14 @@ def main():
     a = int(input("Please enter the number for n: "))
     condition_det(a)
 
+#This function will determine the number of the fibonacci number
 def condition_det(a_number):
     if a_number ==0:
         return 0
     elif a_number == 1 or a_number ==2:
         return 1
     else:
+
         
 
 if __name__ == "__main__":
